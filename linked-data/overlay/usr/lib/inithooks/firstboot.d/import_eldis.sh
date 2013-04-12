cdd5abfaada135c545017551b5f17610a716f779
root <root@core.(none)>
2013-04-12 12:57:08 +0000

    updated eldis import stub to run new code

--- linked-data/overlay/usr/lib/inithooks/firstboot.d/import_eldis.sh
+++ linked-data/overlay/usr/lib/inithooks/firstboot.d/import_eldis.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#turn virtuoso on to be able to insert data
-service  virtuoso-opensource-6.1 start &&
-isql-vt 1111 dba dba /opt/tools/eldis_load.isql 
+python ../use_eldis.py 
 function pause(){
    read -p "$*"
 }
