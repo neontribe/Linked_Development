4d36b1780aae09fcc7c208f119f2042344d4b455
root <root@core.(none)>
2013-04-16 15:40:44 +0000

    updated r4d update code

--- linked-data/overlay/opt/tools/r4d_update.py
+++ linked-data/overlay/opt/tools/r4d_update.py
@@ -1,7 +1,7 @@
 """
 copyright neontribe ltd 2013 neil@neontribe.co.uk
 
-updates eldis data weekly
+updates r4d data weekly
 
 empties /home/r4d/rdf
 get new files
@@ -26,11 +26,11 @@ def main():
     os.system('rm -rf /home/r4d/rdf/*')
     os.system('echo http://r4d.graph.iri > /home/r4d/rdf/global.graph')
     
-    os.system('wget  http://www.dfid.gov.uk/r4d/rdf/R4DOutputsData.zip > /home/r4d/rdf/R4D.zip')
+    os.system('wget  http://www.dfid.gov.uk/r4d/rdf/R4DOutputsData.zip')
+    os.system('mv R4DOutputsData.zip /home/r4d/rdf/R4DOutputsData.zip')
     #unpack r4d data
-    os.system('cd /home/r4d/rdf')
-    os.system('unzip R4D.zip')
-    os.system('rm -f R4D.zip')
+    os.system('unzip /home/r4d/rdf/R4DOutputsData.zip -d /home/r4d/rdf/')
+    os.system('rm -f /home/r4d/rdf/R4DOutputsData.zip')
     #open default conection to isql and run commands in a file
     #get virtuoso password
     fh = open('/etc/virtuoso-opensource-6.1/password', 'r')
