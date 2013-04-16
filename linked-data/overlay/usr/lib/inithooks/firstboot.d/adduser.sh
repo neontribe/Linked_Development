0d1e68563f27488092243b92d21f9ccb5d894e9b
Andy Barnes <andy@neontribe.co.uk>
2013-04-16 14:16:17 +0200

    Update adduser.sh
    added locking/unlocking of account before/after password is set.

--- linked-data/overlay/usr/lib/inithooks/firstboot.d/adduser.sh
+++ linked-data/overlay/usr/lib/inithooks/firstboot.d/adduser.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # Adds non root user for ssh access.
-useradd linked-data -m -s /bin/bash
+
+# create user "linked-data" with home directory /home/linked-data and bash shell
+/usr/sbin/useradd linked-data -m -s /bin/bash
+
+# lock the "linked-data" account until a password is set
+/usr/bin/passwd linked-data -l
+
+# prompt user for password - todo can we write text in the prompt explaing what the password is for?
 /usr/lib/inithooks/bin/setpass.py linked-data
+
+# unlock the "linked-data" account now the password has been set
+/usr/bin/passwd linked-data -u
