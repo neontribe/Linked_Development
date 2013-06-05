fa2dd28f9412059560f6619eb7b071d964754eb0
Toby Batch <tobias@neontribe.co.uk>
2013-06-05 12:59:19 +0100

    Moved symfony into deployment

--- sy2/src/LD/APIBundle/LDAPIBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace LD\APIBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LDAPIBundle extends Bundle
-{
-}
--- sy2/src/LD/DevBundle/LDDevBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace LD\DevBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LDDevBundle extends Bundle
-{
-}
