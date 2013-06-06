8aaad13a50bbe895b74269f84f9064a6a9700461
Andy Barnes <andy@neontribe.co.uk>
2013-06-06 15:22:13 +0200

    moved openapi setup into conf/post-overlay

--- Linked_Development/overlay/opt/openapi/app/ld_api_installer.sh
+++ Linked_Development/overlay/opt/openapi/app/ld_api_installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PWD=`pwd`
-CWD=`dirname $0`/..
-COMPOSER=$CWD/app/composer.phar
-SYUSER=nobody
-SYGROUP=nogroup
-WEBUSER=www-data
-WEBGROUP=www-data
+#PWD=`pwd`
+#CWD=`dirname $0`/..
+#COMPOSER=$CWD/app/composer.phar
+#SYUSER=nobody
+#SYGROUP=nogroup
+#WEBUSER=www-data
+#WEBGROUP=www-data
 
 # ensure system status
 # now integrated in Linked_Development/Linked_Development/conf/pre-overlay - AB
@@ -23,20 +23,20 @@ WEBGROUP=www-data
 # rewrite already enabled
 #a2enmod rewrite
 
-ln -s /opt/openapi/web /var/www/htdocs/openapi
+#ln -s /opt/openapi/web /var/www/htdocs/openapi
 
 # API Setup
 cd $CWD
 # run the vendor install (twice, it occasionally fails half way through the first run)
-php $COMPOSER update
-php $COMPOSER update
+#php $COMPOSER update
+#php $COMPOSER update
 
 # clear caches/logs the agressive way
-rm -rf $CWD/app/{cache,logs}/*
+#rm -rf $CWD/app/{cache,logs}/*
 
 # reset permissions
-chown -R $SYUSER:$SYGROUP $CWD
-chown -R $WEBUSER:$WEBGROUP $CWD/app/{logs,cache}
-chmod -R 1775 $CWD/app/{logs,cache}
+#chown -R $SYUSER:$SYGROUP $CWD
+#chown -R $WEBUSER:$WEBGROUP $CWD/app/{logs,cache}
+#chmod -R 1775 $CWD/app/{logs,cache}
 
-cd $PWD
+#cd $PWD
